fd4bee52d4d409987a7ae65c62929ae035c720d8
Caua <cauaberrnardinolima@gmail.com>
2024-08-07 20:56:50 -0300

    correções vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Head } from "./components/Head/head";
 import { ChatListSearch } from "./components/ChatListSearch/ChatListSearch";
-import { ChatList } from "./components/ChatList/ChatList";
 
 function App() {
   return (
