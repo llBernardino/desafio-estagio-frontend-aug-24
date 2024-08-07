7fd68cbcc09082d663ceb84890914d6849d0e9b4
Caua <cauaberrnardinolima@gmail.com>
2024-08-07 17:56:43 -0300

    datails css

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ChatListSearch } from "./components/ChatListSearch/ChatListSearch";
 
 function App() {
   return (
-    <div className="min-h-screen w-full flex">
+    <div className="min-h-screen w-full flex pl-16 pt-5">
       <div>
         <Head />
         <ChatListSearch />
